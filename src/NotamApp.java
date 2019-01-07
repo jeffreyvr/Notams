a5e4e1b8fd6d3e8d55bf5a5dbb475aacee5178ba
jochemWo <45205721+jochemWo@users.noreply.github.com>
2019-01-07 20:27:48 +0100

    Update NotamApp.java

--- src/NotamApp.java
+++ src/NotamApp.java
@@ -7,6 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NotamApp {
+    
+    /**
+	 * 
+	 */
+	private Chart chart;
+	
+	/**
+	 * 
+	 */
+	public Main() {
+		this.chart = new Chart("Notam Application", 9, 262, 169);
+		chart.init();
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
 
     public List<Notam> createNotamList(){
         XmlInput xmlNotamFile = new XmlInput();
